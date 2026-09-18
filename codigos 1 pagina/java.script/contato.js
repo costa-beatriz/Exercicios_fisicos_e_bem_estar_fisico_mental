cef762191105f6cd594a787b6884243863a5b9a5
juliokubiack <julio.kubiack@estudantes.ifc.edu.br>
2026-09-18 20:25:22 -0300

    commit julio

---
+++ codigos 1 pagina/java.script/contato.js
@@ -0,0 +1,37 @@
+import { supabase } from "./supabaseClient.js";
+ 
+const contatoForm = document.getElementById("contatoForm");
+ 
+contatoForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+ 
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const telefone = document.getElementById("telefone").value;
+  const notas = document.getElementById("notas").value;
+ 
+  const retornoSelecionado = document.querySelector('input[name="retorno"]:checked');
+  const retorno = retornoSelecionado ? retornoSelecionado.value : null;
+ 
+  const { data, error } = await supabase
+    .from("contatos_db")
+    .insert([
+      {
+        nome: nome,
+        email: email,
+        telefone: telefone,
+        mensagem: notas,
+        retorno: retorno
+      }
+    ])
+    .select();
+ 
+  if (error) {
+    alert("Erro ao enviar mensagem: " + error.message);
+    console.log(error);
+    return;
+  }
+ 
+  alert("Mensagem enviada com sucesso! Responderemos em breve.");
+  contatoForm.reset();
+});
